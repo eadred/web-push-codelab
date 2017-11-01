c9a09994da8e1e1ff764121abb7628f2486c1698
Eadred Birchenough <eadred.birchenough@zuhlke.com>
2017-11-01 08:33:06 +0000

    Step (plus using a fixed tag for the notification).

--- public/sw.js
+++ public/sw.js
@@ -20,3 +20,19 @@
 /* eslint-env browser, serviceworker, es6 */
 
 'use strict';
+
+self.addEventListener('push', function (event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data ? event.data.text() : "no data"}"`);
+
+    const title = 'Push Codelab';
+    const options = {
+        body: 'Yay it works.',
+        icon: 'images/icon.png',
+        badge: 'images/badge.png', // Only used by Android?
+        tag: 'FixedTag'
+    };
+
+    const notificationPromise = self.registration.showNotification(title, options);
+    event.waitUntil(notificationPromise);
+});
