434f314ada5cb2fea463e74034ba5305807f607c
Eadred Birchenough <eadred.birchenough@zuhlke.com>
2017-11-01 08:16:21 +0000

    Step 5

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -44,9 +44,20 @@ function urlB64ToUint8Array(base64String) {
 }
 
 function initializeUI() {
+  pushButton.addEventListener('click', function() {
+    pushButton.disabled = true;
+    if (isSubscribed) {
+      // TODO: Unsubscribe
+    } else {
+      subscribeUser();
+    }
+  });
+
   swRegistration.pushManager.getSubscription()
     .then(function(subscription) {
       isSubscribed = !(subscription === null);
+
+      updateSubscriptionOnServer(subscription);
 
       if (isSubscribed) {
         console.log('User IS subscribed.');
@@ -56,6 +67,42 @@ function initializeUI() {
 
       updateBtn();
     });
+}
+
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true, // Must be true
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed.');
+
+    updateSubscriptionOnServer(subscription);
+
+    isSubscribed = true;
+
+    updateBtn();
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+    updateBtn();
+  });
+}
+
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
 }
 
 function updateBtn() {
